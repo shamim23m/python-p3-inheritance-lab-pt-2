249c1917c466d628b25fe8f569bf66812b37d44c
shamim23m <shamimmohamud13@gmail.com>
2024-12-03 09:09:58 +0300

    initial commit

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,17 @@
 class Student:
-    pass
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")  # Correct capitalization
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()  # Call the parent class's hello method
+        print("How are you doing today? I'm okay, but I'm kind of tired. "
+              "Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! "
+              "What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for _ in range(10):  # Raise hand 10 times
+            super().raise_hand()  # Correct capitalization here as well
